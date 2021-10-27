bd93aee28e5d5937b94c0bd4c2355ab19949ec3e
davisstacked <brettzvi@gmail.com>
2021-10-27 13:36:17 -0700

    async functions

--- functions.test.js
+++ functions.test.js
@@ -28,9 +28,42 @@ test('Should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy();
 });
 
+// To Equal
 test('User should be Brad Traversy Object', () => {
   expect(functions.createUser()).toEqual({
     firstName: 'Brad',
     lastName: 'Traversy'
   })
+})
+
+// .toBeLessThan
+// .toBeGreaterThan
+// .toBeLessThanOrEqual
+
+// Less than and Greater than
+test('should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+})
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/i);
+  // lower case i for regex is case insensitive
+})
+
+// Arrays
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'karen', 'admin'];
+  expect(usernames).toContain('admin');
+});
+
+//  Working with Async Data
+test('User fetched name should be Leanne Graham', () => {
+  expect.assertions(1);
+  return functions.fetchUser()
+    .then(data => {
+      expect(data.name).toEqual('Leanne Graham');
+    })
 })
