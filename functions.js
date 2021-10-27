665ecd4aec4d6fd7b7dccdd1c4b3555c9e93d62b
davisstacked <brettzvi@gmail.com>
2021-10-27 16:46:20 -0700

    tutorial complete

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,30 @@
 const functions = require('./functions');
 
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log('Database Initialized...');
+// const closeDatabase = () => console.log('Database Closed...'); 
+
+const nameCheck = () => console.log('Checking Name...');
+
+describe('Checking Names', () => {
+  beforeEach(() => nameCheck());
+
+  test('User is Jeff', () => {
+    const user = 'Jeff';
+    expect(user).toBe('Jeff');
+  })
+
+  test('User is Karen', () => {
+    const user = 'Karen';
+    expect(user).toBe('Karen');
+  })
+});
+
 // .toBe is for testing primitive values
 // if testing an object or an array use .toEqual instead
 
